a975174cb5b85d5fddc44209832d63c41078504c
subhadip420 <subhadippramanik02@gmail.com>
2026-05-22 23:25:16 +0530

    Remove extra code and reformat.

--- lib/widgets/custom_toast.dart
+++ lib/widgets/custom_toast.dart
@@ -3,31 +3,20 @@ import 'package:flutter/material.dart';
 class CustomToast {
   static void show({
     required BuildContext context,
-
     required String message,
-
     required bool isDark,
-
     IconData? icon,
-
     Color? color,
-
     Duration duration = const Duration(seconds: 2),
   }) {
     ScaffoldMessenger.of(context).hideCurrentSnackBar();
-
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         duration: duration,
-
         behavior: SnackBarBehavior.floating,
-
         backgroundColor: Colors.transparent,
-
         elevation: 0,
-
         //margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 14),
-
         //content: Container(
         content: Center(
           child: IntrinsicWidth(
@@ -52,11 +41,8 @@ class CustomToast {
                 boxShadow: [
                   BoxShadow(
                     color: (color ?? Colors.blueAccent).withValues(alpha: 0.25),
-
                     blurRadius: 14,
-
                     spreadRadius: 1,
-
                     offset: const Offset(0, 4),
                   ),
                 ],
@@ -64,21 +50,17 @@ class CustomToast {
 
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   if (icon != null) ...[
                     Container(
                       width: 34,
                       height: 34,
-
                       decoration: BoxDecoration(
                         shape: BoxShape.circle,
-
                         gradient: LinearGradient(
                           colors: [
                             color ?? Colors.blueAccent,
-
                             color ?? Colors.cyanAccent,
                           ],
                         ),
@@ -86,9 +68,7 @@ class CustomToast {
 
                       child: Icon(
                         icon,
-
                         color: Colors.white,
-
                         size: 15,
                       ),
                     ),
@@ -101,9 +81,7 @@ class CustomToast {
                       textAlign: TextAlign.center,
                       style: TextStyle(
                         color: isDark ? Colors.white : Colors.black87,
-
                         fontSize: 14,
-
                         fontWeight: FontWeight.w600,
                       ),
                     ),
